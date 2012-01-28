efcf3e303651545b6f1115a49c997a1c141aa481
Dashamir Hoxha <dashohoxha@gmail.com>
2012-01-28 08:08:42 +0100

    Importing LibreOffice translations.

---
+++ import/import-scripts/import-office-files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### get $data_root and $languages
+. ../config.sh
+
+### import the PO files from GNOME
+for lng in $languages
+do
+    projects=$(ls $data_root/LibreOffice/$lng/)
+    for project in $projects
+    do
+	po_files=$(find $data_root/LibreOffice/$lng/$project -name '*\.po')
+	for file in $po_files
+	do
+	    pot_name=${file#*/source/}
+	    pot_name=${pot_name%.po}
+            #echo $lng, $project, $pot_name, $file;  continue;  ## debug
+	    ../po_import.php LibreOffice $project $pot_name $lng $file
+	done
+    done
+done
