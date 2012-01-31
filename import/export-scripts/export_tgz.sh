48f0c8e584dd28407fca039130dd6ac2e5a3154c
Dashamir Hoxha <dashohoxha@gmail.com>
2012-01-31 21:40:25 +0100

    export_tgz

---
+++ import/export-scripts/export_tgz.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+### Export the translation files of a given origin/project/lng
+### as a tgz archive. Output the path of the created archive.
+### If project==all, then all the projects of the given origin
+### will be exported.
+
+### get the parameters
+if [ $# -lt 3 ]
+then
+    echo "Usage: $0 origin project lng [output_dir]"
+    echo ""
+    exit 1
+fi
+origin=$1
+project=$2
+lng=$3
+output_dir=${4:-/tmp}
+#echo $origin $project $lng $output_dir;  exit;  # debug
+
+### go to the script directory
+cd $(dirname $0)
+
+### get the DB connection parameters
+mysql_params="$($(which php) ../db_params.php bash)"
+#echo $mysql_params;  exit;  # debug
+
+### get the list of the projects to be exported
+if [ "$project" != 'all' ]
+then
+    project_list=$project
+else
+    sql="SELECT project FROM l10n_suggestions_projects WHERE origin = '$origin'"
+    project_list=$(echo $sql | mysql $mysql_params --skip-column-names)
+fi
+
+### export the PO files of all the projects in project_list
+pid=$$
+export_dir=$output_dir/$pid
+for proj in $project_list
+do
+    ### get from the DB the names of the templates and the filenames
+    sql="SELECT t.tplname, f.filename FROM l10n_suggestions_files f
+         LEFT JOIN l10n_suggestions_templates t ON (f.potid = t.potid) 
+         LEFT JOIN l10n_suggestions_projects p ON (t.pguid = p.pguid)
+         WHERE p.origin = '$origin' AND p.project = '$proj' AND f.lng = '$lng'"
+    #echo $sql | mysql $mysql_params --skip-column-names | sed -e 's/\t/,/g' ;  exit;  # debug
+    result_rows=$(echo $sql | mysql $mysql_params --skip-column-names | sed -e 's/\t/,/g')
+
+    ### export all the PO files of the project
+    for row in $result_rows
+    do
+	tplname=$(echo $row | cut -d, -f1)
+	filename=$(echo $row | cut -d, -f2)
+	#echo $origin, $proj, $tplname, $filename;  continue;  # debug
+	po_file=$export_dir/$origin/$filename
+	mkdir -p $(dirname $po_file)
+	echo ../po_export.php $origin $proj $tplname $lng $po_file
+	../po_export.php $origin $proj $tplname $lng $po_file
+    done
+done
+
+### create the tgz archive on the output dir
+tgz_file=$output_dir/$origin-$project-$lng.tgz
+tar -C $export_dir --create --gzip --file=$tgz_file $origin
+
+### clean up
+rm -rf $export_dir
+
+### output the path/name of the tgz file
+echo $tgz_file
