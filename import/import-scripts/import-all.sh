00a1d090369d5a102f245a47ffa48c771829775f
Dashamir Hoxha <dashohoxha@gmail.com>
2012-02-02 20:57:55 +0100

    Update

--- import/import-scripts/import-all.sh
+++ import/import-scripts/import-all.sh
@@ -10,3 +10,7 @@ cd $(dirname $0)
 ./import-kde-files.sh
 ./import-ubuntu-templates.sh
 ./import-ubuntu-files.sh
+./import-office-templates.sh
+./import-office-files.sh
+./import-mozilla-templates.sh
+./import-mozilla-files.sh
