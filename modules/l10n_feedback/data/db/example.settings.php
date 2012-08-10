f2691507296ee7e3d55c882de1ce4d5d3b7c55e2
Dashamir Hoxha <dashohoxha@gmail.com>
2012-08-10 11:09:43 +0200

    Renamed db connection files.

---
+++ modules/l10n_feedback/data/db/example.settings.php
@@ -0,0 +1,7 @@
+<?php
+$dbdriver = 'mysql';
+$dbhost   = 'localhost';
+$dbname   = 'l10n_dev';
+$dbuser   = 'l10n_dev';
+$dbpass   = 'l10n_dev';
+?>
--- modules/l10n_feedback/data/db/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbdriver = 'mysql';
-$dbhost   = 'localhost';
-$dbname   = 'test1';
-$dbuser   = 'test1';
-$dbpass   = 'test1';
-?>
