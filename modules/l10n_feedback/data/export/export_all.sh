aa8887a26f0ea4fabccd89db1f58f695defcecca
Dashamir Hoxha <dashohoxha@gmail.com>
2012-12-14 05:39:04 +0100

    Export projects periodically by cron.

---
+++ modules/l10n_feedback/data/export/export_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+### Export all the projects that are listed in a file.
+### Usually called by cron.
+
+### get the parameters
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 output_dir project_list.txt
+project_list.txt contains lines of the form origin/project/lng
+(where project can also be 'all')
+"
+    echo ""
+    exit 1
+fi
+output_dir=$1
+project_list=$2
+
+### go to the script directory
+cd $(dirname $0)
+
+### export each project that is on the list
+while read line
+do
+    origin=$(echo $line | cut -d'/' -f1)
+    project=$(echo $line | cut -d'/' -f2)
+    lng=$(echo $line | cut -d'/' -f3)
+    #echo ./export_tgz.sh $origin $project $lng ; continue  ## debug
+    if [ "$project" = '' ]; then continue; fi
+    ./export_tgz.sh $origin $project $lng
+    filename=$origin-$project-$lng
+    mv -f /tmp/$filename.tgz $output_dir/$filename.tgz
+done < $project_list
+
