1b8215eef7863e5e6dc0b5e799e10cd68b426eba
Dashamir Hoxha <dashohoxha@gmail.com>
2013-04-18 06:45:34 +0200

    Improved the installation script and instructions.

--- install/install.sh
+++ install/install.sh
@@ -54,25 +54,13 @@ mount -o bind /proc $target_dir/proc
 chroot $target_dir apt-get update
 chroot $target_dir apt-get -y install ubuntu-minimal
 
-
 ### apply to chroot the scripts and the overlay
 cwd=$(dirname $0)
 chroot $target_dir mkdir -p /tmp/install
 cp -a $cwd/* $target_dir/tmp/install/
 chroot $target_dir /tmp/install/install-scripts/00-config.sh
-chroot $target_dir /tmp/install/config.sh
-chroot $target_dir rm -rf /tmp/install
 
-### shutdown services
-services="php5-fpm memcached mysql nginx"
-for srv in $services
-do
-    chroot $target_dir service $srv stop
-done
-
-umount $target_dir/proc
-
-### make an init script and start it at boot
+### create an init script and make it start at boot
 cd $target_dir
 chroot_dir=$(pwd)
 cd $cwd
@@ -80,3 +68,10 @@ init_script="/etc/init.d/chroot-$(basename $chroot_dir)"
 sed -e "/^CHROOT=/c CHROOT='$chroot_dir'" $cwd/init.sh > $init_script
 chmod +x $init_script
 update-rc.d $(basename $init_script) defaults
+
+### customize the configuration of the chroot system
+#chroot $target_dir /tmp/install/config.sh
+#chroot $target_dir rm -rf /tmp/install
+
+### stop the services inside chroot
+#$init_script stop
