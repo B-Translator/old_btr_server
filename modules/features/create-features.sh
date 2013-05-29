5dfeeaf8a556a5d480eaa55df436048d13587af5
Dashamir Hoxha <dashohoxha@gmail.com>
2013-05-29 13:47:26 +0000

    Disqus features.

--- modules/features/create-features.sh
+++ modules/features/create-features.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_layout $(cat layout.txt)
-drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_conte
-nt node_export_features
+drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_content node_export_features
+drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_disqus $(cat disqus.txt)
+
+lng=$(drush @dev vget l10n_feedback_translation_lng --exact --format=json)
+lng=${lng:1:-1}  # remove the double quotes
+drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_$lng $(cat custom.txt)
