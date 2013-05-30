65c1a23df94c34505c7df0f03ca31ec0e278a4af
Dashamir Hoxha <dashohoxha@gmail.com>
2013-05-30 12:59:11 +0000

    Sharethis feature.

--- modules/features/create-features.sh
+++ modules/features/create-features.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_layout $(cat layout.txt)
-drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_content node_export_features
-drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_disqus $(cat disqus.txt)
+drush="drush --yes @dev"
+destination=--destination=profiles/btranslator/modules/features
 
-lng=$(drush @dev vget l10n_feedback_translation_lng --exact --format=json)
-lng=${lng:1:-1}  # remove the double quotes
-drush --yes @dev fe --destination=profiles/btranslator/modules/features btranslator_$lng $(cat custom.txt)
+$drush fe $destination btranslator_layout $(cat layout.txt)
+$drush fe $destination btranslator_content node_export_features
+
+$drush fe $destination btranslator_disqus $(cat disqus.txt)
+$drush fe $destination btranslator_sharethis $(cat sharethis.txt)
