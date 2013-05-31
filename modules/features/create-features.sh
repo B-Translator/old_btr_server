b1886277fd09422db4c183b9190e3bf437f4492e
Dashamir Hoxha <dashohoxha@gmail.com>
2013-05-31 04:28:23 +0000

    Added Janrain feature.

--- modules/features/create-features.sh
+++ modules/features/create-features.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 drush="drush --yes @dev"
-destination=--destination=profiles/btranslator/modules/features
+destination="--destination=profiles/btranslator/modules/features"
 
 $drush fe $destination btranslator_layout $(cat layout.txt)
 $drush fe $destination btranslator_content node_export_features
 
 $drush fe $destination btranslator_disqus $(cat disqus.txt)
 $drush fe $destination btranslator_sharethis $(cat sharethis.txt)
+$drush fe $destination btranslator_janrain $(cat janrain.txt)
