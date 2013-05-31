9165c4ab1268946716f7956d28ec38972a1e150f
Dashamir Hoxha <dashohoxha@gmail.com>
2013-05-31 10:36:51 +0000

    Added features mass_contact and invite.

--- modules/features/create-features.sh
+++ modules/features/create-features.sh
@@ -2,12 +2,15 @@
 
 drush="drush --yes @dev"
 destination="--destination=profiles/btranslator/modules/features"
+features_export="$drush features-export $destination"
 
-$drush fe $destination btranslator_layout $(cat layout.txt)
-$drush fe $destination btranslator_content node_export_features
+$features_export btranslator_layout $(cat layout.txt)
+$features_export btranslator_content node_export_features
 
-$drush fe $destination btranslator_disqus $(cat disqus.txt)
-$drush fe $destination btranslator_sharethis $(cat sharethis.txt)
-$drush fe $destination btranslator_janrain $(cat janrain.txt)
-$drush fe $destination btranslator_drupalchat $(cat drupalchat.txt)
-$drush fe $destination btranslator_simplenews $(cat simplenews.txt)
+$features_export btranslator_disqus $(cat disqus.txt)
+$features_export btranslator_sharethis $(cat sharethis.txt)
+$features_export btranslator_janrain $(cat janrain.txt)
+$features_export btranslator_drupalchat $(cat drupalchat.txt)
+$features_export btranslator_simplenews $(cat simplenews.txt)
+$features_export btranslator_mass_contact $(cat mass_contact.txt)
+$features_export btranslator_invite $(cat invite.txt)
