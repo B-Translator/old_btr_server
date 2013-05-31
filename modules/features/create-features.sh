b3c580d8dd4a3955cb4542e48411f75ac4cece03
Dashamir Hoxha <dashohoxha@gmail.com>
2013-05-31 05:02:12 +0000

    Added feature drupalchat.

--- modules/features/create-features.sh
+++ modules/features/create-features.sh
@@ -9,3 +9,4 @@ $drush fe $destination btranslator_content node_export_features
 $drush fe $destination btranslator_disqus $(cat disqus.txt)
 $drush fe $destination btranslator_sharethis $(cat sharethis.txt)
 $drush fe $destination btranslator_janrain $(cat janrain.txt)
+$drush fe $destination btranslator_drupalchat $(cat drupalchat.txt)
