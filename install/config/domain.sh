0153d2fd94718cfe207003a1dd779cb924870018
Dashamir Hoxha <dashohoxha@gmail.com>
2013-07-17 00:17:23 +0200

    Fixed domain name

--- install/config/domain.sh
+++ install/config/domain.sh
@@ -9,8 +9,9 @@ This is the domain that you have registered
 It will modify the files:
  1) /etc/hostname
  2) /etc/hosts
- 3) /etc/nginx/sites-available/default
- 4) /var/www/btranslator/sites/default/settings.php
+ 3) /etc/nginx/sites-available/*
+ 4) /etc/apache2/sites-available/*
+ 5) /var/www/btranslator*/sites/default/settings.php
 "
 FQDN='l10n.org.xx'
 read -p "Enter the domain [$FQDN]: " input
@@ -18,19 +19,19 @@ FQDN=${input:-$FQDN}
 
 echo $FQDN > /etc/hostname
 sed -i /etc/hosts \
-    -e "/^127.0.1.1/c 127.0.1.1 $FQDN btranslator"
+    -e "/^127.0.0.1/c 127.0.0.1 $FQDN localhost"
 
 for file in $(ls /etc/nginx/sites-available/*)
 do
     sed -i $file -e "s/server_name .*\$/server_name $FQDN;/"
 done
 
-for file in $(ls /var/www/btranslator*/sites/default/settings.php)
-do
-    sed -i $file -e "/^\\\$base_url/c \$base_url = \"https://$FQDN\";"
-done
-
 for file in $(ls /etc/apache2/sites-available/*)
 do
     sed -i $file -e "s/ServerName .*\$/ServerName $FQDN/"
+done
+
+for file in $(ls /var/www/btranslator*/sites/default/settings.php)
+do
+    sed -i $file -e "/^\\\$base_url/c \$base_url = \"https://$FQDN\";"
 done
