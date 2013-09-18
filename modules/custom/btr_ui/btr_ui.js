b3fc672a6777778cda2067649f41b91287967ef0
Dashamir Hoxha <dashohoxha@gmail.com>
2013-09-19 00:54:34 +0200

    Renaming module l10n_feedback to btr_ui.

--- modules/custom/btr_ui/btr_ui.js
+++ modules/custom/btr_ui/btr_ui.js
@@ -1,11 +1,11 @@
 
-Drupal.behaviors.replaceL10nServerURLs = function(context) {
+Drupal.behaviors.replaceBtrUiURLs = function(context) {
   // Act on links if we have URL replacements to do.
-  if (Drupal.settings.l10nServerURLs) {
-    for (var url in Drupal.settings.l10nServerURLs) {
+  if (Drupal.settings.btrUiURLs) {
+    for (var url in Drupal.settings.btrUiURLs) {
       // Look for links with this exact URL and replace with extended version.
       // This ensures we keep filter values while switching tabs.
-      $('a[href=' + url +']', context).not('.filter-exclude').attr('href', Drupal.settings.l10nServerURLs[url]);
+      $('a[href=' + url +']', context).not('.filter-exclude').attr('href', Drupal.settings.btrUiURLs[url]);
     }
   }
 }
