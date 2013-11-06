b5cb65e61ed1b4d12e626a11ef7e65b3a16ffac4
Dashamir Hoxha <dashohoxha@gmail.com>
2013-11-06 13:55:38 +0100

    Fixing the public resource uri in examples.

--- modules/custom/btrServices/test/report/retrieve.php
+++ modules/custom/btrServices/test/report/retrieve.php
@@ -2,12 +2,12 @@
 <?php
 
 // general contribution statistics
-$url = 'https://dev.l10n.org.xx/btr/report/statistics?lng=sq';
+$url = 'https://dev.l10n.org.xx/public/btr/report/statistics?lng=sq';
 $response = drupal_http_request($url);
 print_r($response);
 
 // top contributors
-$url = 'https://dev.l10n.org.xx/btr/report/topcontrib'
+$url = 'https://dev.l10n.org.xx/public/btr/report/topcontrib'
   . '?lng=sq&period=week&size=5';
 $options = array(
   'headers' => array('Accept' => 'application/xml'),
--- modules/custom/btrServices/test/report/retrieve.sh
+++ modules/custom/btrServices/test/report/retrieve.sh
@@ -2,8 +2,8 @@
 
 ### general contribution statistics
 curl -k -i -X GET -H "Accept: application/json"  \
-     "https://dev.l10n.org.xx/btr/report/statistics?lng=sq"
+     "https://dev.l10n.org.xx/public/btr/report/statistics?lng=sq"
 
 ### top contributors
 curl -k -i -X GET -H "Accept: application/json"  \
-     "https://dev.l10n.org.xx/btr/report/topcontrib?lng=sq&period=week&size=5"
+     "https://dev.l10n.org.xx/public/btr/report/topcontrib?lng=sq&period=week&size=5"
