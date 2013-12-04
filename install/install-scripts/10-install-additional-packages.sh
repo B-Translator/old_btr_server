1f36d1781502fca39105a093022dec55a6615611
Dashamir Hoxha <dashohoxha@gmail.com>
2013-12-04 00:57:16 +0000

    Implementing some of the tips in this article: http://codeinthehole.com/writing/pull-requests-and-other-good-practices-for-teams-using-github/

--- install/install-scripts/10-install-additional-packages.sh
+++ install/install-scripts/10-install-additional-packages.sh
@@ -22,8 +22,12 @@ install aptitude tasksel vim nano psmisc language-pack-en
 install mysql-server ssmtp memcached php5-memcached \
         php5-mysql php5-gd php-db php5-dev make php-pear php5-curl php-apc \
         ssl-cert gawk unzip wget diff curl phpmyadmin \
-        git mercurial subversion translate-toolkit
+        git mercurial subversion translate-toolkit ruby
 install screen logwatch
+
+### install hub: http://hub.github.com/
+curl http://hub.github.com/standalone -sLo /bin/hub
+chmod +x /bin/hub
 
 ### phpmyadmin will install apache2 and start it
 ### so we should stop and disable it
