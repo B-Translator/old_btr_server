7fc364d7e892b54cc3534168d9d037870fecfb11
Dashamir Hoxha <dashohoxha@gmail.com>
2014-01-21 08:49:35 +0100

    Send a tweet each day.

---
+++ utils/twitter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+### Send tweets from command line.
+### For installation instructions see:
+### http://xmodulo.com/2013/12/access-twitter-command-line-linux.html
+
+base_url=https://l10n.org.al
+tweet="$(wget --no-check-certificate $base_url/translations/twitter/sq -O-)"
+mention=$(t following | sort -R | tail -1)
+t update "$tweet @$mention"
