aae6449ebd2fbedc4c4b9070558488630a3a3014
Dashamir Hoxha <dashohoxha@gmail.com>
2014-01-24 08:00:08 +0000

    Fixed the twitter cron and script.

--- utils/twitter.sh
+++ utils/twitter.sh
@@ -3,7 +3,8 @@
 ### For installation instructions see:
 ### http://xmodulo.com/2013/12/access-twitter-command-line-linux.html
 
+t='/usr/local/bin/t'
 base_url=https://l10n.org.al
 tweet="$(wget --no-check-certificate $base_url/translations/twitter/sq -O-)"
-mention=$(t following | sort -R | tail -1)
-t update "$tweet @$mention"
+mention=$($t following | sort -R | tail -1)
+$t update "$tweet @$mention"
