6d5ea94c8e04d6f0687006774d1a8b66fdb26e8a
Dashamir Hoxha <dashohoxha@gmail.com>
2014-02-19 03:25:45 +0000

    Fixed missing GitHub provider (for hybridauth).

--- install/install-scripts/20-make-and-install-btranslator.sh
+++ install/install-scripts/20-make-and-install-btranslator.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash -x
 
 ### retrieve all the projects/modules and build the application directory
@@ -7,10 +8,8 @@ drush make --prepare-install --force-complete \
            --contrib-destination=profiles/btranslator \
            $makefile $drupal_dir
 cp -a $drupal_dir/profiles/btranslator/{libraries/bootstrap,themes/contrib/bootstrap/}
-#cp -a $drupal_dir/profiles/btranslator/{libraries/hybridauth-2.1.2/hybridauth,libraries/}
-#cp $drupal_dir/profiles/btranslator/libraries/hybridauth-additional-providers-1.8/hybridauth-github/Providers/GitHub.php $drupal_dir/profiles/btranslator/libraries/hybridauth/Hybrid/Providers/
-#cp $drupal_dir/profiles/btranslator/libraries/hybridauth-additional-providers-1.8/hybridauth-github/Providers/DrupalOAuth2.php $drupal_dir/profiles/btranslator/libraries/hybridauth/Hybrid/Providers/
 cp $drupal_dir/profiles/btranslator/libraries/hybridauth/{additional-providers/hybridauth-drupaloauth2/Providers/DrupalOAuth2.php,hybridauth/Hybrid/Providers/}
+cp $drupal_dir/profiles/btranslator/libraries/hybridauth/{additional-providers/hybridauth-github/Providers/GitHub.php,hybridauth/Hybrid/Providers/}
 
 ### create the directory of PO files
 mkdir -p /var/www/PO_files
