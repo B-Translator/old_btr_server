db2da72e85b62c672174eed105db091e40141124
Dashamir Hoxha <dashohoxha@gmail.com>
2014-03-02 06:57:58 +0000

    Example scripts for the API.

---
+++ modules/custom/btrServices/test/translations_sguid.php
@@ -0,0 +1,53 @@
+<?php
+include_once(dirname(__FILE__) . '/config.php');
+include_once(dirname(__FILE__) . '/http_request.php');
+
+// POST public/btr/translations/get_random_sguid
+$url = $base_url . '/public/btr/translations/get_random_sguid';
+$options = array(
+  'method' => 'POST',
+  'data' => array(
+    'target' => 'next',
+  ),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+  ),
+);
+$result = http_request($url, $options);
+
+$options = array(
+  'method' => 'POST',
+  'data' => array(
+    'target' => 'translated',
+    'lng' => 'sq',
+  ),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+  ),
+);
+$result = http_request($url, $options);
+
+$options = array(
+  'method' => 'POST',
+  'data' => array(
+    'target' => 'translated',
+    'lng' => 'sq',
+    'last_sguid' => 'c7ac448a01c566680d8ffb00430a55ffc779f24b',
+  ),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+  ),
+);
+$result = http_request($url, $options);
+
+$options = array(
+  'method' => 'POST',
+  'data' => array(
+    'target' => 'next',
+    'last_sguid' => 'c7ac448a01c566680d8ffb00430a55ffc779f24b',
+  ),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+  ),
+);
+$result = http_request($url, $options);
