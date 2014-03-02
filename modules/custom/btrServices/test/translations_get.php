48f37c47e11e99335a8e4f6f04489d3fc64dc631
Dashamir Hoxha <dashohoxha@gmail.com>
2014-03-02 19:45:37 +0000

    Added service translations/get

---
+++ modules/custom/btrServices/test/translations_get.php
@@ -0,0 +1,26 @@
+<?php
+include_once(dirname(__FILE__) . '/config.php');
+include_once(dirname(__FILE__) . '/http_request.php');
+
+// POST public/btr/translations/get
+$url = $base_url . '/public/btr/translations/get';
+$options = array(
+  'method' => 'POST',
+  'data' => array(
+    'sguid' => 'ed685775fa0608fa42e20b3d28454c63972f62cd',
+    'lng' => 'sq',
+  ),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+  ),
+);
+$result = http_request($url, $options);
+
+$options['data']['sguid'] = 'next';
+$result = http_request($url, $options);
+
+$options['data']['sguid'] = 'translated';
+$result = http_request($url, $options);
+
+$options['data']['sguid'] = 'untranslated';
+$result = http_request($url, $options);
