f9ff7bcfc800bc66af187f1f23ebb1e3baf8603a
Dashamir Hoxha <dashohoxha@gmail.com>
2014-03-02 13:06:24 +0000

    Example scripts for the API.

---
+++ modules/custom/btrServices/test/translations_submit.php
@@ -0,0 +1,51 @@
+<?php
+include_once(dirname(__FILE__) . '/config.php');
+include_once(dirname(__FILE__) . '/get_access_token.php');
+include_once(dirname(__FILE__) . '/http_request.php');
+
+// Get an access  token.
+$access_token = get_access_token($auth);
+
+// Actions that will be submitted.
+$actions = array(
+  array(
+    'action' => 'add',
+    'params' => array(
+      'sguid' => 'd66b0fc286b887e9242ee1e6b777522f067f92af',
+      'lng' => 'sq',
+      'translation' => 'Test translation.',
+    ),
+  ),
+  array(
+    'action' => 'vote',
+    'params' => array('tguid' => '40af5f58a7d1211c0cb5950d0b36b21c06cf50e6'),
+  ),
+  array(
+    'action' => 'del',
+    'params' => array('tguid' => 'test-f58a7d1211c0cb5950d0b36b21c06cf50e6'),
+  ),
+  array(
+    'action' => 'del_vote',
+    'params' => array('tguid' => 'test-f58a7d1211c0cb5950d0b36b21c06cf50e6'),
+  ),
+  array(
+    'action' => 'add',
+    'params' => array(
+      'sguid' => 'd68b68585ee36d0bcda3dd3fd6eb4ebc2cdcbcbd',
+      'lng' => 'sq',
+      'translation' => '',
+    ),
+  ),
+);
+
+// POST btr/translations/submit
+$url = $base_url . '/btr/translations/submit';
+$options = array(
+  'method' => 'POST',
+  'data' => $actions,
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+    'Authorization' => 'Bearer ' . $access_token,
+  ),
+);
+$result = http_request($url, $options);
--- modules/custom/btrServices/test/translations_vote.php
+++ modules/custom/btrServices/test/translations_vote.php
@@ -33,3 +33,15 @@ $result = http_request($url, $options);
 // Retrive the string and check that the translation has been voted.
 $url = $base_url . "/public/btr/translations/$sguid?lng=sq";
 $result = http_request($url);
+
+// POST btr/translations/del_vote
+$url = $base_url . '/btr/translations/del_vote';
+$options = array(
+  'method' => 'POST',
+  'data' => array('tguid' => $tguid),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+    'Authorization' => 'Bearer ' . $access_token,
+  ),
+);
+$result = http_request($url, $options);
