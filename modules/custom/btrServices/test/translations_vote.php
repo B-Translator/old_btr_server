f81c7922d1541e5ec3f490712ef6de1a8ffa440d
Dashamir Hoxha <dashohoxha@gmail.com>
2014-03-02 09:23:28 +0000

    Example scripts for the API.

---
+++ modules/custom/btrServices/test/translations_vote.php
@@ -0,0 +1,35 @@
+<?php
+include_once(dirname(__FILE__) . '/config.php');
+include_once(dirname(__FILE__) . '/get_access_token.php');
+include_once(dirname(__FILE__) . '/http_request.php');
+
+// Get a random translated string.
+$url = $base_url . '/public/btr/translations/translated?lng=sq';
+$result = http_request($url);
+
+// Get the tguid of the first translation.
+foreach ((array)$result as $sguid => $string) {
+  foreach ((array)$string->translations as $tguid => $translation) {
+    break;
+  }
+  break;
+}
+
+// Get an access  token.
+$access_token = get_access_token($auth);
+
+// POST btr/translations/vote
+$url = $base_url . '/btr/translations/vote';
+$options = array(
+  'method' => 'POST',
+  'data' => array('tguid' => $tguid),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+    'Authorization' => 'Bearer ' . $access_token,
+  ),
+);
+$result = http_request($url, $options);
+
+// Retrive the string and check that the translation has been voted.
+$url = $base_url . "/public/btr/translations/$sguid?lng=sq";
+$result = http_request($url);
