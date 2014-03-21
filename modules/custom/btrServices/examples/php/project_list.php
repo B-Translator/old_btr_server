e61f29c126d4192f265b906d83f3719186a813b3
Dashamir Hoxha <dashohoxha@gmail.com>
2014-03-21 17:40:09 +0000

    Adding project resources.

---
+++ modules/custom/btrServices/examples/php/project_list.php
@@ -0,0 +1,26 @@
+<?php
+include_once(dirname(__FILE__) . '/config.php');
+include_once(dirname(__FILE__) . '/http_request.php');
+
+// POST public/btr/project/list
+$url = $base_url . '/public/btr/project/list';
+$options = array(
+  'method' => 'POST',
+  'data' => array(),
+  'headers' => array(
+    'Content-type' => 'application/x-www-form-urlencoded',
+  ),
+);
+$result = http_request($url, $options);
+
+// Filter list by origin.
+$options['data'] = array('origin' => 't*');
+$result = http_request($url, $options);
+
+// Retrieve only a list of origins.
+$options['data'] = array('project' => '-');
+$result = http_request($url, $options);
+
+// Filter list by origin.
+$options['data'] = array('origin' => 'test', 'project' => 'p*');
+$result = http_request($url, $options);
