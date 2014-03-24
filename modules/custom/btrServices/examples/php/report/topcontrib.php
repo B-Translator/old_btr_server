7f29ac609f9480f7556ac8d6f792b12708194a83
Dashamir Hoxha <dashohoxha@gmail.com>
2014-03-24 21:03:49 +0000

    Separated stats examples into two files.

--- modules/custom/btrServices/examples/php/report/topcontrib.php
+++ modules/custom/btrServices/examples/php/report/topcontrib.php
@@ -3,15 +3,18 @@ $path = dirname(dirname(__FILE__));
 include_once($path . '/config.php');
 include_once($path . '/http_request.php');
 
-// GET public/btr/report/statistics
-$url = $base_url . '/public/btr/report/statistics?lng=sq';
+// GET public/btr/report/topcontrib
+$url = $base_url . '/public/btr/report/topcontrib?lng=sq&period=week';
 $result = http_request($url);
 
-// POST public/btr/report/statistics
-$url = $base_url . '/public/btr/report/statistics';
+// POST public/btr/report/topcontrib
+$url = $base_url . '/public/btr/report/topcontrib';
 $options = array(
   'method' => 'POST',
-  'data' => array('lng' => 'sq'),
+  'data' => array(
+    'lng' => 'sq',
+    'period' => 'week',
+  ),
   'headers' => array(
     'Content-type' => 'application/x-www-form-urlencoded',
   ),
