5b462f2b90c1190fc759e49adef0d410cde69172
Dashamir Hoxha <dashohoxha@gmail.com>
2014-05-04 20:47:44 +0200

    Replaced the old github path with the new one.

--- install/install-scripts/20-make-and-install-btranslator.sh
+++ install/install-scripts/20-make-and-install-btranslator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 ### retrieve all the projects/modules and build the application directory
-makefile="https://raw.github.com/dashohoxha/B-Translator/master/build-btranslator.make"
+makefile="https://raw.github.com/B-Translator/server/master/build-btranslator.make"
 rm -rf $drupal_dir
 drush make --prepare-install --force-complete \
            --contrib-destination=profiles/btranslator \
--- install/install-scripts/25-git-clone-btranslator.sh
+++ install/install-scripts/25-git-clone-btranslator.sh
@@ -7,7 +7,7 @@
 ### clone btranslator from github
 cd $drupal_dir/profiles/
 mv btranslator btranslator-bak
-git clone https://github.com/dashohoxha/B-Translator btranslator
+git clone https://github.com/B-Translator/server btranslator
 
 ### copy contrib libraries and modules
 cp -a btranslator-bak/libraries/ btranslator/
