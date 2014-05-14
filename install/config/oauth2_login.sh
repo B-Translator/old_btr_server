0b0d472d9859398fb702574e739e936ce662631c
Dashamir Hoxha <dashohoxha@gmail.com>
2014-05-14 11:31:03 +0200

    Fixed oauth2 login script.

--- install/config/oauth2_login.sh
+++ install/config/oauth2_login.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-### paths of server config and client config scripts
-btr=/var/www/btr/profiles/btr_server/install/config
-bcl=/var/www/bcl/profiles/btr_client/install/config
+echo "===> Making configurations for OAuth2 Login"
 
 ### get the domain name of btr_client and btr_server
 btrclient=$(grep ' localhost' /etc/hosts | head -n 1 | cut -d' ' -f2)
@@ -16,13 +14,16 @@ redirect_url=https://$btrclient/oauth2/authorized
 skip_ssl=1
 
 ### register an oauth2 client on btr_server
-drush --yes @btr php-script $btr/register_oauth2_client.php  \
+btr=/var/www/btr/profiles/btr_server/install/config
+$btr/mysqld.sh start
+drush --yes @btr php-script --script-path=$btr register_oauth2_client.php  \
     "$client_id" "$client_secret" "$redirect_url"
 drush @btr cc all
 
 ### setup oauth2 login on btr_client
+bcl=/var/www/bcl/profiles/btr_client/install/config
 $bcl/mysqld.sh start
-drush --yes @bcl php-script $bcl/oauth2_login.php  \
+drush --yes @bcl php-script --script-path=$bcl oauth2_login.php  \
     "$server_url" "$client_id" "$client_secret" "$skip_ssl"
 drush @bcl cc all
 
