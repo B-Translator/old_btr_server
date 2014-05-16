104d227ae06ba056d09c1197e3d0ff8d237a7de9
Dashamir Hoxha <dashohoxha@gmail.com>
2014-05-16 18:26:23 +0200

    Fixed the reinstall script.

--- dev/reinstall.sh
+++ dev/reinstall.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-### Reinstall btr_server from scratch.
+### Reinstall btr and bcl from scratch.
 ### Useful for testing installation scripts.
 
-### Reinstall btr_server.
+### get config settings from a file
+if [ "$1" = '' ]
+then
+    echo "Usage: $0 settings.sh"
+    exit 1
+fi
+settings=$1
+set -a
+source  $settings
+set +a
+
+### Reinstall btr_client.
 
 export drupal_dir=/var/www/btr
 export drush="drush --root=$drupal_dir"
 
+rm -rf $drupal_dir-bak
 mv $drupal_dir $drupal_dir-bak
 
-cd $(dirname $0)
-cd ../install/install-scripts/
-
+cd /var/www/github/btr_server/install/install-scripts/
 ./20-make-and-install-btrserver.sh
 ./25-git-clone-btrserver.sh
 ./30-separate-translation-data.sh
@@ -22,13 +32,15 @@ cd ../install/install-scripts/
 export drupal_dir=/var/www/bcl
 export drush="drush --root=$drupal_dir"
 
+rm -rf $drupal_dir-bak
 mv $drupal_dir $drupal_dir-bak
 
-cd /var/www/bcl_dev/install/install-scripts/
+cd /var/www/github/btr_client/install/install-scripts/
 ./20-make-and-install.sh
 ./30-git-clone.sh
 ./40-configure.sh
 
 ### Configure (both /var/www/btr/ and /var/www/bcl/).
-../config.sh
+cd /var/www/github/btr_server/install/
+./config.sh
 
