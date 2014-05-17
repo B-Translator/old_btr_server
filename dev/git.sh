1cd2e17f023b921696805110e1b38fd6e64d6e7b
Dashamir Hoxha <dashohoxha@gmail.com>
2014-05-17 18:58:53 +0200

    Script for handling all git repos.

---
+++ dev/git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+### Useful for updating or checking the status of all git repositories.
+### For example:
+###    dev/git.sh status --short
+###    dev/git.sh pull
+
+options=${@:-status --short}
+gitrepos="
+    /var/www/bcl*/profiles/btr_client
+    /var/www/btr*/profiles/btr_server
+    /var/www/github/btr_*
+"
+for repo in $gitrepos
+do
+    echo
+    echo "===> $repo"
+    cd $repo
+    git $options
+done
