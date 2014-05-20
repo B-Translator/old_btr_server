04d2da27120cd7397aa2be1cd6126a02e6cc0841
Dashamir Hoxha <dashohoxha@gmail.com>
2014-05-20 06:50:36 +0200

    Fixing scripts.

--- install/scripts/install-btr-client.sh
+++ install/scripts/install-btr-client.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+### export drupal_dir
 export drupal_dir=/var/www/bcl
 export drush="drush --root=$drupal_dir"
 
-### run install scripts of btr_client
+### go to the directory of scripts
 cd $code_dir/btr_client/install/scripts/
-./20-make-and-install.sh
-./30-git-clone.sh
-./40-configure.sh
+
+### make and install the drupal profile 'btr_client'
+./drupal-make-and-install.sh
+
+### additional configurations related to drupal
+./drupal-config.sh
