9a3084f1c51200753651c04585514d263acfcdec
Dashamir Hoxha <dashohoxha@gmail.com>
2014-07-03 16:47:44 +0000

    Fixed php config for apache2.

--- install/scripts/system-config.sh
+++ install/scripts/system-config.sh
@@ -63,7 +63,17 @@ apc.mmap_file_mask=/tmp/apc.XXXXXX
 apc.shm_size=96M
 EOF
 
+sed -i /etc/php5/apache2/php.ini \
+    -e '/^;\?memory_limit/ c memory_limit = 200M' \
+    -e '/^;\?max_execution_time/ c max_execution_time = 90' \
+    -e '/^;\?display_errors/ c display_errors = On' \
+    -e '/^;\?post_max_size/ c post_max_size = 16M' \
+    -e '/^;\?cgi\.fix_pathinfo/ c cgi.fix_pathinfo = 1' \
+    -e '/^;\?upload_max_filesize/ c upload_max_filesize = 16M' \
+    -e '/^;\?default_socket_timeout/ c default_socket_timeout = 90'
+
 sed -i /etc/php5/fpm/php.ini \
+    -e '/^;\?memory_limit/ c memory_limit = 200M' \
     -e '/^;\?max_execution_time/ c max_execution_time = 90' \
     -e '/^;\?display_errors/ c display_errors = On' \
     -e '/^;\?post_max_size/ c post_max_size = 16M' \
