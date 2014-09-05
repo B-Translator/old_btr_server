8a0d701678c6f94ffab192dad0960e0f97b10512
Dashamir Hoxha <dashohoxha@gmail.com>
2014-09-05 20:57:28 +0000

    Fixed test scripts.

--- modules/custom/btrCore/data/test/import-kturtle.sh
+++ modules/custom/btrCore/data/test/import-kturtle.sh
@@ -1,40 +1,22 @@
-#!/bin/bash
-### Used for testing.
+#!/bin/bash -x
 
 ### go to the script directory
 cd $(dirname $0)
 
-### get $data_root
-. ../config.sh
+### set drush alias
+drush_alias=${1:-@btr}
+drush="drush $drush_alias"
 
-### Files that are imported are assumed to be
-### under the directory '$data_root/$origin/'.
-### The path under $origin does not matter (can
-### be any path that suits the project structure).
+### set some variables
 origin=test
 project=kturtle
-po_dir="$data_root/$origin/$project"
-rm -rf $po_dir/
-mkdir -p $po_dir/
-cp po_files/kturtle-fr.po $po_dir/ 
-cp po_files/kturtle-sq.po $po_dir/ 
+path=$(pwd)/po_files
 
-### include snapshot functions
-. ../import/make-snapshot.sh
+### create the project
+$drush btrp-add $origin $project $path/kturtle-fr.po
 
-### make last snapshots before re-import
-make-last-snapshot $origin $project fr
-make-last-snapshot $origin $project sq
-
-### import the template
-potemplate=kturtle
-../import/pot_import.php $origin $project $potemplate $po_dir/kturtle-fr.po
-
-### import the PO files
-../import/po_import.php $origin $project $potemplate fr $po_dir/kturtle-fr.po
-../import/po_import.php $origin $project $potemplate sq $po_dir/kturtle-sq.po
-
-## make initial snapshots after (re)import
-make-snapshot $origin $project fr $po_dir/kturtle-fr.po
-make-snapshot $origin $project sq $po_dir/kturtle-sq.po
-
+### import the PO files of each language
+for lng in fr sq
+do
+    $drush btrp-import $origin $project $lng $path/kturtle-$lng.po
+done
--- modules/custom/btrCore/data/test/import-pingus.sh
+++ modules/custom/btrCore/data/test/import-pingus.sh
@@ -1,42 +1,22 @@
-#!/bin/bash
-### The PO files of Pingus are synchronized with Launchpad:
-### https://translations.launchpad.net/pingus/trunk/+pots/pingus/fr/+translate
-### https://translations.launchpad.net/pingus/trunk/+pots/pingus/sq/+translate
+#!/bin/bash -x
 
 ### go to the script directory
 cd $(dirname $0)
 
-### get $data_root
-. ../config.sh
+### set drush alias
+drush_alias=${1:-@btr}
+drush="drush $drush_alias"
 
-### Files that are imported are assumed to be
-### under the directory '$data_root/$origin/'.
-### The path under $origin does not matter (can
-### be any path that suits the project structure).
+### set some variables
 origin=test
 project=pingus
-po_dir="$data_root/$origin/$project"
-rm -rf $po_dir/
-mkdir -p $po_dir/
-cp po_files/pingus-fr.po $po_dir/ 
-cp po_files/pingus-sq.po $po_dir/ 
+path=$(pwd)/po_files
 
-### include snapshot functions
-. ../import/make-snapshot.sh
+### create the project
+$drush btrp-add $origin $project $path/pingus-fr.po
 
-### make last snapshots before re-import
-make-last-snapshot $origin $project fr
-make-last-snapshot $origin $project sq
-
-### import the template
-potemplate=pingus
-../import/pot_import.php $origin $project $potemplate $po_dir/pingus-fr.po
-
-### import the PO files
-../import/po_import.php $origin $project $potemplate fr $po_dir/pingus-fr.po
-../import/po_import.php $origin $project $potemplate sq $po_dir/pingus-sq.po
-
-## make initial snapshots after (re)import
-make-snapshot $origin $project fr $po_dir/pingus-fr.po
-make-snapshot $origin $project sq $po_dir/pingus-sq.po
-
+### import the PO files of each language
+for lng in fr sq
+do
+    $drush btrp-import $origin $project $lng $path/pingus-$lng.po
+done
