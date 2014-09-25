f868baa5695acbc446a993492e05186ff6477836
Dashamir Hoxha <dashohoxha@gmail.com>
2014-09-25 09:35:01 +0200

    Building a docker image.

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+### This file can be used to build a docker image like this:
+###   docker build --tag='btr_server' .
+### It can also be used for automated builds in Docker Hub
+### (see https://docs.docker.com/userguide/dockerrepos/#automated-builds)
+
+FROM ubuntu:14.04
+
+### Install packages first, in order to take advantage
+### of caching features of `docker build`.
+COPY install/scripts/packages-and-software.sh /tmp/
+RUN DEBIAN_FRONTEND=noninteractive /tmp/packages-and-software.sh
+
+### Copy the source code and run the installer.
+COPY . /usr/local/src/btr_server/
+ENV code_dir /usr/local/src/btr_server
+WORKDIR /usr/local/src/btr_server/
+RUN ["install/install-container.sh", "install/settings.sh"]
+
+### Set the default command to run in the container.
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf", "--nodaemon"]
+
--- install/scripts/packages-and-software.sh
+++ install/scripts/packages-and-software.sh
@@ -4,19 +4,17 @@
 apt-get update
 apt-get -y upgrade
 
-install='apt-get -y -o DPkg::Options::=--force-confdef -o DPkg::Options::=--force-confold install'
-
 ### install localization
-$install language-pack-en
+apt-get -y install language-pack-en
 update-locale
 
 ### install other needed packages
-$install aptitude tasksel vim nano psmisc cron supervisor
-$install mysql-server ssmtp memcached php5-memcached \
+apt-get -y install aptitude tasksel vim nano psmisc cron supervisor
+apt-get -y install mysql-server ssmtp memcached php5-memcached \
          php5-mysql php5-gd php-db php5-dev php-pear php5-curl php-apc \
          make ssl-cert gawk unzip wget curl diffutils phpmyadmin git ruby \
          mercurial subversion translate-toolkit dtrx
-$install screen logwatch
+apt-get -y install screen logwatch
 initctl reload-configuration
 
 ### install hub: http://hub.github.com/
@@ -25,7 +23,7 @@ chmod +x /bin/hub
 
 ### install twitter cli client
 ### see also: http://xmodulo.com/2013/12/access-twitter-command-line-linux.html
-$install ruby-dev
+apt-get -y install ruby-dev
 gem install t
 
 ### phpmyadmin will install apache2 and start it
@@ -34,7 +32,7 @@ gem install t
 update-rc.d apache2 disable
 
 ### install nginx and php5-fpm
-$install nginx nginx-common nginx-full php5-fpm
+apt-get -y install nginx nginx-common nginx-full php5-fpm
 
 ### There is some problem with php-pear in 14.04
 ### See: http://askubuntu.com/questions/451953/php-pear-is-not-working-after-upgrading-to-ubuntu-14-04
