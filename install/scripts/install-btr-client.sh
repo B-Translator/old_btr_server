e89dc999e18496b8126c4f7da0b16389f83ad7dc
Dashamir Hoxha <dashohoxha@gmail.com>
2014-09-25 18:44:47 +0000

    Fixed installation with `docker build`.

--- install/scripts/install-btr-client.sh
+++ install/scripts/install-btr-client.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+### make sure that we have the code of btr_client
+if ! test -d /usr/local/src/btr_client
+then
+    cd /usr/local/src/
+    git clone --branch=$bcl_git_branch https://github.com/B-Translator/btr_client
+fi
+
 ### export drupal_dir
 export drupal_dir=/var/www/bcl
 export drush="drush --root=$drupal_dir"
