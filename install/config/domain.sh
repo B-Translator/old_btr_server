c36a53692cc53606b1241d6b10832b79f528a188
Dashamir Hoxha <dashohoxha@gmail.com>
2014-10-11 05:40:39 +0200

    Fixed domains.

--- install/config/domain.sh
+++ install/config/domain.sh
@@ -17,31 +17,32 @@ It will modify the files:
  8) /var/www/btr*/sites/default/settings.php
 "
 
+### get the current domains
+old_bcl_domain=$(head -n 1 /etc/hosts.conf | cut -d' ' -f2)
+old_bcl_domain=${old_bcl_domain:-example.org}
+old_domain=$(head -n 1 /etc/hosts.conf | cut -d' ' -f3)
+old_domain=${old_domain:-btr.example.org}
+
+### get the new domains
 if [ -z "${bcl_domain+xxx}" -o "$bcl_domain" = '' ]
 then
-    bcl_domain='example.org'
-    read -p "Enter the domain name for btr_client [$bcl_domain]: " input
-    bcl_domain=${input:-$bcl_domain}
+    read -p "Enter the domain name for btr_client [$old_bcl_domain]: " input
+    bcl_domain=${input:-$old_bcl_domain}
 fi
 
 if [ -z "${domain+xxx}" -o "$domain" = '' ]
 then
-    domain='btr.example.org'
-    read -p "Enter the domain name for btr_server [$domain]: " input
-    domain=${input:-$domain}
+    read -p "Enter the domain name for btr_server [$old_domain]: " input
+    domain=${input:-$old_domain}
 fi
 
-### get the current domains
-old_bcl_domain=$(head -n 1 /etc/hosts.conf | cut -d' ' -f2)
-old_domain=$(head -n 1 /etc/hosts.conf | cut -d' ' -f3)
-
-### change /etc/hostname and /etc/hosts.conf
+### update /etc/hostname and /etc/hosts.conf
 echo $bcl_domain > /etc/hostname
 sed -i /etc/hosts.conf \
     -e "1c 127.0.0.1 $bcl_domain $domain"
 /etc/hosts_update.sh
 
-### change config files for the client
+### update config files for the client
 for file in $(ls /etc/nginx/sites-available/bcl*)
 do
     sed -i $file -e "/server_name/ s/$old_bcl_domain/$bcl_domain/"
@@ -57,7 +58,7 @@ do
     sed -i $file -e "/^\\\$base_url/ s/$old_bcl_domain/$bcl_domain/"
 done
 
-### change config files for the server
+### update config files for the server
 for file in $(ls /etc/nginx/sites-available/btr*)
 do
     sed -i $file -e "/server_name/ s/$old_domain/$domain/"
