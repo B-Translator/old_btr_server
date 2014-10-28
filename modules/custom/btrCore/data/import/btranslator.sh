c9054e967aecf063bacc1f64a4ddaea39e13b7df
Dashamir Hoxha <dashohoxha@gmail.com>
2014-10-28 19:36:05 +0000

    Fixed the script for importing B-Translator translations.

--- modules/custom/btrCore/data/import/btranslator.sh
+++ modules/custom/btrCore/data/import/btranslator.sh
@@ -4,21 +4,17 @@
 ### go to the script directory
 cd $(dirname $0)
 
-### set the drush alias
-drush="drush $1"
+### get config vars
+. ../config.sh
 
-### set some variables
-drupal_dir=$($drush dd)
-l10n_dir=$drupal_dir/profiles/btr_server/l10n
-origin=Drupal
-project=btranslator
-
-### import the POT file
-$drush btrp-add $origin $project $l10n_dir/btrserver.pot
+### import the POT files
+code_dir='/usr/local/src'
+$drush btrp-add Drupal btr_server $code_dir/btr_server/l10n/btrserver.pot
+$drush btrp-add Drupal btr_client $code_dir/btr_client/l10n/btrclient.pot
 
 ### import the PO file of each language
-languages="sq"
 for lng in $languages
 do
-    $drush btrp-import $origin $project $lng $l10n_dir/btrserver.$lng.po
+    $drush btrp-import Drupal btr_server $lng $code_dir/btr_server/l10n/btrserver.$lng.po
+    $drush btrp-import Drupal btr_client $lng $code_dir/btr_client/l10n/btrclient.$lng.po
 done
