41e0e4fdf6051620f21307be4e454a7bb295cd7f
Dashamir Hoxha <dashohoxha@gmail.com>
2015-02-05 21:07:05 +0000

    Fixed comment watcher.

--- utils/watch-comments.sh
+++ utils/watch-comments.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ### Watch the latest Disqus comments for l10n-sq
 ### and notify the relevant users about them.
+### Run it on background like this:
+###     utils/watch-comments.php &
 
 cd $(dirname $0)
 
 url='http://l10n-sq.disqus.com/latest.rss'
-rsstail="rsstail -u $url -1 -l -d -i 15 -n 0"
+rsstail="rsstail -u $url -l -d -n 0"
 $rsstail | ./watch-comments.php
 
