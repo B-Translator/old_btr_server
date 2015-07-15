4b685c0376651073f7fa8b07ca5add2166888a88
Dashamir Hoxha <dashohoxha@gmail.com>
2015-07-15 09:53:54 +0000

    Fix rss-feeds

---
+++ modules/custom/btrCore/lib/fn/utils/shorten.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Function: utils_shorten()
+ */
+
+namespace BTranslator;
+
+/**
+ * Shorten the given string.
+ *
+ * From the given (possibly long) string, returns a short string
+ * of the given length that can be suitable for title, subject, etc.
+ */
+function utils_shorten($string, $length) {
+  $str = strip_tags(str_replace("\n", ' ', $string));
+  if (strlen($str) > $length) {
+    $str = substr($str, 0, strrpos(substr($str, 0, $length - 3), ' '));
+    $str .= '...';
+  }
+  $str = utf8_decode($str);
+  $str = htmlentities($str);
+  return $str;
+}
