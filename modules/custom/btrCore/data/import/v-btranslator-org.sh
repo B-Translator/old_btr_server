6e27d39c13c492eaceb45b37b7244a6ff3d84bb9
Dashamir Hoxha <dashohoxha@gmail.com>
2015-07-18 09:22:39 +0000

    Import the translations of the vocabulary mobile app.

---
+++ modules/custom/btrCore/data/import/v-btranslator-org.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+### Import the translations of https://github.com/B-Translator/vocabulary-jquery
+
+### go to the script directory
+cd $(dirname $0)
+
+### get config vars
+. ../config.sh
+
+### get the po files
+rm -rf $data_root/vocabulary-jquery/
+git clone https://github.com/B-Translator/vocabulary-jquery $data_root/vocabulary-jquery/
+
+### set some variables
+origin=dashohoxha
+project=v.btranslator.org
+
+### create the project and import the PO files of each language
+echo -e "\n==========> $origin $project"
+drush @btr btrp-add $origin $project $data_root/vocabulary-jquery/l10n/app.pot
+drush @btr btrp-import $origin $project sq $data_root/vocabulary-jquery/l10n/po/sq.po
+drush @btr btrp-import $origin $project de $data_root/vocabulary-jquery/l10n/po/de.po
+
+### set the author of translations
+drush @btr btr-vote --user="Dashamir Hoxha" sq $data_root/vocabulary-jquery/l10n/po/sq.po
+drush @btr btr-vote --user="OpenSrcKansas"  de $data_root/vocabulary-jquery/l10n/po/de.po
