4f3ad1706e42512ef08e0cac4465c8f29ed7600b
Dashamir Hoxha <dashohoxha@gmail.com>
2015-08-08 06:36:15 +0000

    Count the number of translations and votes.

--- modules/custom/btrCore/lib/fn/report/project_stats.php
+++ modules/custom/btrCore/lib/fn/report/project_stats.php
@@ -79,8 +79,29 @@ function report_project_stats($origin, $project, $lng) {
   $stats['nr_untranslated_strings'] = btr::db_query($sql, $args)->fetchField();
 
   // Get the total number of translations.
+  $sql = "
+    SELECT COUNT(t.tguid)
+    FROM {btr_projects} p
+    INNER JOIN {btr_templates} tpl ON (tpl.pguid = p.pguid)
+    INNER JOIN {btr_locations} l ON (l.potid = tpl.potid)
+    INNER JOIN {btr_strings} s ON (s.sguid = l.sguid)
+    INNER JOIN {btr_translations} t ON (t.sguid = s.sguid AND t.lng = :lng)
+    WHERE p.origin = :origin AND p.project = :project
+  ";
+  $stats['nr_translations'] = btr::db_query($sql, $args)->fetchField();
 
   // Get the total number of votes.
+  $sql = "
+    SELECT COUNT(t.tguid)
+    FROM {btr_projects} p
+    INNER JOIN {btr_templates} tpl ON (tpl.pguid = p.pguid)
+    INNER JOIN {btr_locations} l ON (l.potid = tpl.potid)
+    INNER JOIN {btr_strings} s ON (s.sguid = l.sguid)
+    INNER JOIN {btr_translations} t ON (t.sguid = s.sguid AND t.lng = :lng)
+    INNER JOIN {btr_votes} v ON (v.tguid = t.tguid)
+    WHERE p.origin = :origin AND p.project = :project
+  ";
+  $stats['nr_votes'] = btr::db_query($sql, $args)->fetchField();
 
   // Get the number of subscribed users.
 
