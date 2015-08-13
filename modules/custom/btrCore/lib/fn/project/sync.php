7b1e3d766579cf56fcd7439563dbaa1b027cb1d7
Dashamir Hoxha <dashohoxha@gmail.com>
2015-08-13 20:48:07 +0000

    Reorganize the function for project sync.

---
+++ modules/custom/btrCore/lib/fn/project/sync.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @file
+ * Definition of the function project_sync().
+ */
+
+namespace BTranslator;
+use \btr;
+
+/**
+ * Synchronize the translations of a given origin/project/lng with the upstream
+ * project.
+ *
+ * Does not allow concurrent synchronizations because they may affect the
+ * performance of the server.
+ *
+ * @param origin
+ *   Origin of the project.
+ *
+ * @param project
+ *   The name of the project.
+ *
+ * @param lng
+ *   Translation to be exported.
+ */
+function project_sync($origin, $project, $lng) {
+  // The output is plain text.
+  header('Content-Type: text/plain');
+
+  // Make sure that the given origin and project do exist.
+  if (!btr::utils_project_exists($origin, $project)) {
+    print t("The project '!project' does not exist.",
+      ['!project' => "$origin/$project"]);
+    drupal_exit();
+  }
+
+  // Get the sync command.
+  if ($lng=='sq' and $origin=='LibreOffice' and in_array($project, ['sw', 'cui']))
+    {
+      $sync_cmd ="/var/www/data/sync/sq-libo_ui.sh $project 2>&1";
+    }
+  else {
+    print t("There is no script for synchronizing '!project'.",
+      ['!project' => "$origin/$project/$lng"]);
+    drupal_exit();
+  }
+
+  // Try to avoid concurrent synchronizations because they
+  // may affect the performance of the server.
+  if (!lock_acquire('sync_project')) {
+    print t("Error: Server is currently busy. Please try to synchronize again later.");
+    drupal_exit();
+  }
+
+  // Synchronize the project.
+  print "command: $sync_cmd \n";
+  print "Synchronizing '$origin/$project/$lng', please wait... \n\n";
+  flush();
+  set_time_limit(0);
+  //passthru($sync_cmd);
+  $handle = popen($sync_cmd, 'r');
+  while (!feof($handle)) {
+    print fgets($handle);
+    flush();
+    ob_flush();
+  }
+  pclose($handle);
+  print "\nDONE\n\n";
+
+  // Stop Drupal execution right here.
+  lock_release('sync_project');
+  drupal_exit();
+}
