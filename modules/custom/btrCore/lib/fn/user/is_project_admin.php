b90eeb7f3aa94309cb8b25ba0cab3fbacd1207af
Dashamir Hoxha <dashohoxha@gmail.com>
2015-08-14 18:44:08 +0000

    Bug fix

--- modules/custom/btrCore/lib/fn/user/is_project_admin.php
+++ modules/custom/btrCore/lib/fn/user/is_project_admin.php
@@ -15,10 +15,11 @@ function user_is_project_admin($origin, $project, $lng = NULL) {
   if (user_access('btranslator-admin'))  return TRUE;
 
   // Check that the project language matches translation_lng of the user.
-  if ($lng !== NULL and $lng != $GLOBALS['user']->translation_lng) return FALSE;
+  $user = user_load($GLOBALS['user']->uid);
+  if ($lng !== NULL and $lng != $user->translation_lng) return FALSE;
 
   // Check whether the user is an admin of the given project.
-  if (in_array("$origin/$project", $GLOBALS['user']->admin_projects)) return TRUE;
+  if (in_array("$origin/$project", $user->admin_projects)) return TRUE;
 
   // Otherwise he cannot administrate.
   return FALSE;
