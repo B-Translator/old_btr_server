a87c080df815e4533e84016ede14ecf700b3114b
Dashamir Hoxha <dashohoxha@gmail.com>
2015-08-16 05:03:49 +0000

    Create functions for subscribe/unsubscribe.

---
+++ modules/custom/btrCore/lib/fn/project/subscriptions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * Definition of function project_subscriptions()
+ */
+
+namespace BTranslator;
+
+/**
+ * Return the list of projects to which the user is subscribed.
+ *
+ * @param $uid
+ *   (Optional) ID of the user.
+ */
+function project_subscriptions($uid = NULL) {
+  if ($uid===NULL)  $uid = $GLOBALS['user']->uid;
+  $account = user_load($uid);
+
+  $subscribed_projects = array();
+  $projects = $account->field_preferred_projects[LANGUAGE_NONE];
+  foreach($projects as $p) {
+    $subscribed_projects[] = $p['value'];
+  }
+
+  return $subscribed_projects;
+}
