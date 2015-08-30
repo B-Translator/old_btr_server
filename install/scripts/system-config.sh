d43bab9f8e350d31ac631a439fe602590ba2632b
Dashamir Hoxha <dashohoxha@gmail.com>
2015-08-30 11:55:03 +0000

    Fix installation scripts.

--- install/scripts/system-config.sh
+++ install/scripts/system-config.sh
@@ -47,14 +47,19 @@ ln -sf /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
 a2enconf api-examples downloads phpmyadmin
 
 sed -i /etc/php5/apache2/php.ini \
-    -e '/^\[PHP\]/ a apc.rfc1867 = 1' \
-    -e '/^display_errors/ c display_errors = On'
+    -e '/^\[PHP\]/ a apc.rfc1867 = 1'
 
-sed -i /etc/apache2/mods-available/mpm_prefork.conf \
-    -e '/^<IfModule/,+5 s/StartServers.*/StartServers 2/' \
-    -e '/^<IfModule/,+5 s/MinSpareServers.*/MinSpareServers 2/' \
-    -e '/^<IfModule/,+5 s/MaxSpareServers.*/MaxSpareServers 4/' \
-    -e '/^<IfModule/,+5 s/MaxRequestWorkers.*/MaxRequestWorkers 50/'
+if [ "$development" = 'true' ]
+then
+    sed -i /etc/php5/apache2/php.ini \
+	-e '/^display_errors/ c display_errors = On'
+
+    sed -i /etc/apache2/mods-available/mpm_prefork.conf \
+	-e '/^<IfModule/,+5 s/StartServers.*/StartServers 2/' \
+	-e '/^<IfModule/,+5 s/MinSpareServers.*/MinSpareServers 2/' \
+	-e '/^<IfModule/,+5 s/MaxSpareServers.*/MaxSpareServers 4/' \
+	-e '/^<IfModule/,+5 s/MaxRequestWorkers.*/MaxRequestWorkers 50/'
+fi
 
 ### modify the configuration of php5
 cat <<EOF > /etc/php5/conf.d/apc.ini
