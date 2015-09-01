c92b6b374bd08d792db2ef65ba3d8cdbb91ecf75
Dashamir Hoxha <dashohoxha@gmail.com>
2015-09-01 16:53:02 +0000

    More docker scripts

---
+++ docker/create.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+cd $(dirname $0)
+source ./config
+ssh=${ssh:2201}
+
+docker stop $container
+docker rm $container
+
+if [ "$dev" = 'false' ]
+then
+    ### create a container for production
+    docker create --name=$container --hostname=$hostname \
+        -p 80:80 -p 443:443 -p $ssh:$ssh $image
+else
+    ### create a container for development
+    docker create --name=$container $image
+    docker start $container
+    docker cp $container:/var/www/btr/profiles/btr_server $(pwd)/
+    docker cp $container:/var/www/bcl/profiles/btr_client $(pwd)/
+    docker stop $container
+    docker rm $container
+
+    let ssh1=ssh+1
+    docker create --name=$container --hostname=$hostname \
+        -v $(pwd)/btr_server:/var/www/btr/profiles/btr_server \
+        -v $(pwd)/btr_client:/var/www/bcl/profiles/btr_client \
+        -p 81:80 -p 444:443 -p $ssh1:$ssh \
+        $image
+fi
