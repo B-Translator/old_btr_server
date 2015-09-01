7f9ec81e60cdf04f9e6f121da2efffd051e93ea8
Dashamir Hoxha <dashohoxha@gmail.com>
2015-09-01 09:47:43 +0000

    More script fixing and improvements.

--- install/config/fix_file_permissions.sh
+++ install/config/fix_file_permissions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 ### drush may create some files with wrong (root) permissions
 ### fix them (change owner to www-data)
 
@@ -6,3 +6,5 @@ for dir in /var/www/{btr,bcl}*/sites/default/files/*
 do
     test -d $dir && chown www-data: -R $dir
 done
+
+exit 0  # it is ok even if something above failed
