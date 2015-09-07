0de4654be9c10052c73311c0478f17d7ab9cc1a5
Dashamir Hoxha <dashohoxha@gmail.com>
2015-09-07 13:48:28 +0000

    Customize the pager (next, previous, etc.)

--- themes/btr_server/template.php
+++ themes/btr_server/template.php
@@ -4,3 +4,35 @@
  * @file
  * template.php
  */
+
+/**
+ * Implement hook: theme_pager_previous()
+ */
+function btr_server_pager_previous($variables) {
+  if ($variables['text'] == 'previous' ) $variables['text'] = '‹';
+  return theme_pager_previous($variables);
+}
+
+/**
+ * Implement hook: theme_pager_next()
+ */
+function btr_server_pager_next($variables) {
+  if ($variables['text'] == 'next' ) $variables['text'] = '›';
+  return theme_pager_next($variables);
+}
+
+/**
+ * Implement hook: theme_pager_first()
+ */
+function btr_server_pager_first($variables) {
+  if ($variables['text'] == 'first' ) $variables['text'] = '«';
+  return theme_pager_first($variables);
+}
+
+/**
+ * Implement hook: theme_pager_last()
+ */
+function btr_server_pager_last($variables) {
+  if ($variables['text'] == 'last' ) $variables['text'] = '»';
+  return theme_pager_last($variables);
+}
