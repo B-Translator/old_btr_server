903c7bc45c2f9cae10e30f30efd9a1ba16270c7b
Dashamir Hoxha <dashohoxha@gmail.com>
2015-09-30 03:33:10 +0000

    Improve docker/create.sh

--- docker/create.sh
+++ docker/create.sh
@@ -6,37 +6,38 @@ ssh=${ssh:-2201}
 docker stop $container
 docker rm $container
 
+### Remove the given directory if it exists.
+function remove_dir() {
+    local dir=$1
+    if test -d $dir/
+    then
+        cd $dir/
+        if test -n "$(git status --porcelain)"
+        then
+            echo "Directory $dir/ cannot be removed because it has uncommited changes."
+            exit 1
+        fi
+        cd ..
+        rm -rf $dir/
+    fi
+}
+
 if [ "$dev" = 'false' ]
 then
+    mkdir -p downloads uploads exports
+
     ### create a container for production
     docker create --name=$container --hostname=$hostname \
-        -p 80:80 -p 443:443 -p $ssh:$ssh $image
+        -v /data/PO_files:/var/www/data \
+        -v $(pwd)/downloads:/var/www/downloads \
+        -v $(pwd)/uploads:/var/www/uploads \
+        -v $(pwd)/exports:/var/www/exports \
+        -p 80:80 -p 443:443 -p $ssh:$ssh \
+        $image
 else
-    ### remove the directory btr_client/ if it exists
-    if test -d btr_client/
-    then
-        cd btr_client/
-        if test -n "$(git status --porcelain)"
-        then
-            echo "Directory btr_client/ cannot be removed because it has uncommited changes."
-            exit 1
-        fi
-        cd ..
-        rm -rf btr_client/
-    fi
-
-    ### remove the directory btr_server/ if it exists
-    if test -d btr_server/
-    then
-        cd btr_server/
-        if test -n "$(git status --porcelain)"
-        then
-            echo "Directory btr_server/ cannot be removed because it has uncommited changes."
-            exit 1
-        fi
-        cd ..
-        rm -rf btr_server/
-    fi
+    ### remove the directories btr_client/ and btr_server/ if they exist
+    remove_dir btr_client
+    remove_dir btr_server
 
     ### create a container for development
     docker create --name=$container $image
@@ -50,6 +51,7 @@ else
     docker create --name=$container --hostname=$hostname \
         -v $(pwd)/btr_server:/var/www/btr/profiles/btr_server \
         -v $(pwd)/btr_client:/var/www/bcl/profiles/btr_client \
+        -v /data/PO_files:/var/www/data \
         -p 81:80 -p 444:443 -p $ssh1:$ssh \
         $image
 fi
