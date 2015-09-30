1e7d12beb79f5f713dbc652092eea0c8c512b921
Dashamir Hoxha <dashohoxha@gmail.com>
2015-09-30 17:25:04 +0200

    Improve docker scripts and update README.

--- docker/create.sh
+++ docker/create.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 source ./config
-ssh=${ssh:-2201}
 
 docker stop $container
 docker rm $container
@@ -24,22 +23,20 @@ function remove_dir() {
 
 if [ "$dev" = 'false' ]
 then
-    mkdir -p downloads uploads exports
-
     ### create a container for production
+    mkdir -p downloads uploads exports
     docker create --name=$container --hostname=$hostname \
         -v /data/PO_files:/var/www/data \
         -v $(pwd)/downloads:/var/www/downloads \
         -v $(pwd)/uploads:/var/www/uploads \
         -v $(pwd)/exports:/var/www/exports \
-        -p 80:80 -p 443:443 -p $ssh:$ssh \
-        $image
+        $ports $image
 else
     ### remove the directories btr_client/ and btr_server/ if they exist
     remove_dir btr_client
     remove_dir btr_server
 
-    ### create a container for development
+    ### copy directories btr_client/ and btr_server/ from the image to the host
     docker create --name=$container $image
     docker start $container
     docker cp $container:/var/www/btr/profiles/btr_server $(pwd)/
@@ -47,11 +44,11 @@ else
     docker stop $container
     docker rm $container
 
-    let ssh1=ssh+1
+    ### create a container for development, sharing diectories
+    ### btr_client/ and btr_server/ between the container and the host
     docker create --name=$container --hostname=$hostname \
         -v $(pwd)/btr_server:/var/www/btr/profiles/btr_server \
         -v $(pwd)/btr_client:/var/www/bcl/profiles/btr_client \
         -v /data/PO_files:/var/www/data \
-        -p 81:80 -p 444:443 -p $ssh1:$ssh \
-        $image
+        $ports $image
 fi
