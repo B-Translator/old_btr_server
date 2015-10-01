29d907c73f71c6e92f94f6a09bf6053681feae5e
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-01 02:35:46 +0000

    Create a vocabulary for each language.

--- modules/custom/btrCore/data/import/vocabulary.sh
+++ modules/custom/btrCore/data/import/vocabulary.sh
@@ -1,28 +1,20 @@
 #!/bin/bash 
 ### Import the vocabulary projects.
 
-### go to the script directory
-cd $(dirname $0)
-
 ### set drush site
 drush="drush $1"
 
-for file in $(ls vocabulary/*_*.po)
+### go to the script directory
+cd $(dirname $0)
+
+### get $languages
+source ../config.sh
+
+### create a vocabulary for each language
+for lng in $languages
 do
-    ### get vocabulary and lng from the name of the PO file
-    filename=$(basename $file)
-    name=${filename%.po}
-    lng=${name##*_}
-    vocabulary=${name%_*}
+    if test $($drush btrv-ls --name=ICT_$lng) ; then continue ; fi
 
-    if test $($drush btrv-ls --name=$name)
-    then
-        echo "Vocabulary $name already exists; skipping."
-        continue
-    fi
-
-    echo "Importing $name."
-    ### create a vocabulary and import translations
-    $drush btrv-add $vocabulary $lng $(pwd)/$file
-    #$drush btr-vote-import --user=dasho $lng $(pwd)/$file
+    echo "Creating vocabulary ICT_$lng."
+    $drush btrv-add ICT $lng
 done
