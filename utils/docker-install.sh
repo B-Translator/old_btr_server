0069cadcfd2fc4b5a1ad7fc9458333251dd6f86a
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-08 09:24:50 +0200

    Use variables for srcdir and workdir.

--- utils/docker-install.sh
+++ utils/docker-install.sh
@@ -5,14 +5,16 @@
 set -e
 
 ### make directories for source code and workdir
-mkdir -p /opt/src /opt/workdir
+srcdir=/opt/src
+workdir= /opt/workdir
+mkdir -p $srcdir $workdir
 
 ### make sure that the script is called with `nohup nice ...`
 if [ "$1" != "--dont-fork" ]
 then
     # this script should be called recursively by itself
     datestamp=$(date +%F | tr -d -)
-    nohup_out=/opt/workdir/nohup-btr_server-$datestamp.out
+    nohup_out=$workdir/nohup-btr_server-$datestamp.out
     rm -f $nohup_out
     nohup nice $0 --dont-fork $@ > $nohup_out &
     sleep 1
@@ -24,45 +26,45 @@ else
 fi
 
 ### get wsproxy
-if test -d /opt/src/wsproxy
+if test -d $srcdir/wsproxy
 then
-    cd /opt/src/wsproxy
+    cd $srcdir/wsproxy
     git pull
 else
-    cd /opt/src/
+    cd $srcdir/
     git clone https://github.com/docker-build/wsproxy
 fi
 
-if ! test -d /opt/src/wsproxy
+if ! test -d $srcdir/wsproxy
 then
     ### create a link on workdir
-    cd /opt/workdir/
-    ln -sf /opt/src/wsproxy .
+    cd $workdir/
+    ln -sf $srcdir/wsproxy .
 
     ### build and run wsproxy
-    cd /opt/workdir/
+    cd $workdir/
     wsproxy/rm.sh 2>/dev/null
     wsproxy/build.sh
     wsproxy/run.sh
 fi
 
 ### get btr_server
-if test -d /opt/src/btr_server
+if test -d $srcdir/btr_server
 then
-    cd /opt/src/btr_server
+    cd $srcdir/btr_server
     git pull
 else
-    cd /opt/src/
+    cd $srcdir/
     git clone https://github.com/B-Translator/btr_server
 fi
 
 ### create a link on workspace
-mkdir -p /opt/workdir/btr
-cd /opt/workdir/btr/
-ln -sf /opt/src/btr_server/docker .
+mkdir -p $workdir/btr
+cd $workdir/btr/
+ln -sf $srcdir/btr_server/docker .
 
 ### build the image
-cd /opt/workdir/
+cd $workdir/
 btr/docker/build.sh --dont-fork $@
 
 ### create and start the container
