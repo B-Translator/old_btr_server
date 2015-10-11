28ee0f71b8f19ff2ecb51795c7019617ad55754c
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-11 23:07:11 +0000

    Rename functions btr::string_add() and btr::string_del() to btr::vocabulary_string_add() and btr::vocabulary_string_del()

---
+++ modules/custom/btrCore/lib/fn/vocabulary/string_add.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * @file
+ * Definition of function vocabulary_string_add().
+ */
+
+namespace BTranslator;
+use \btr;
+
+/**
+ * Add a new string to a vocabulary.
+ *
+ * @param $name
+ *   The name of the vocabulary.
+ *
+ * @param $lng
+ *   The language of the vocabulary.
+ *
+ * @param $string
+ *   String to be added.
+ *
+ * @param $uid
+ *   Id of the user that is adding the string.
+ *
+ * @param $notify (optional)
+ *   It TRUE, notify translators about the new string.
+ *
+ * @return
+ *   The $sguid of the new string.
+ */
+function vocabulary_string_add($name, $lng, $string, $uid, $notify = FALSE) {
+  // Set some variables.
+  $origin = 'vocabulary';
+  $project = $name . '_' . $lng;
+  $tplname = $project;
+  $context = $project;
+  $sguid = sha1($string . $context);
+
+  // Insert a new string.
+  $q = 'SELECT sguid FROM {btr_strings} WHERE sguid = :sguid';
+  $field_sguid = btr::db_query($q, [':sguid' => $sguid])->fetchField();
+  if (!empty($field_sguid)) {
+    $notify = FALSE;
+  }
+  else {
+    btr::db_insert('btr_strings')
+      ->fields([
+          'string' => $string,
+          'context' => $context,
+          'sguid' => $sguid,
+          'uid' => $uid,
+          'time' => date('Y-m-d H:i:s', REQUEST_TIME),
+          'count' => 0,
+        ])
+      ->execute();
+  }
+
+  // Get the template id.
+  $query = 'SELECT potid FROM {btr_templates} WHERE pguid = :pguid AND tplname = :tplname';
+  $args = [':pguid' => sha1($origin . $project), ':tplname' => $tplname];
+  $potid = btr::db_query($query, $args)->fetchField();
+
+  // Insert a new location.
+  $query = 'SELECT lid FROM {btr_locations} WHERE sguid = :sguid AND potid = :potid';
+  $args = [':sguid' => $sguid, ':potid' => $potid];
+  $lid = btr::db_query($query, $args)->fetchField();
+  if (!empty($lid)) {
+    $notify = FALSE;
+  }
+  else {
+    btr::db_insert('btr_locations')
+      ->fields(['sguid' => $sguid, 'potid' => $potid])
+      ->execute();
+  }
+
+  // Insert the string to the materialized view.
+  $table = 'btr_mv_' . strtolower($project);
+  btr::db_delete($table)->condition('string', $string)->execute();
+  btr::db_insert($table)->fields(['string' => $string])->execute();
+
+  // Notify users about the new string.
+  if ($notify) {
+    _btr_new_string_notification($project, $string, $sguid, $uid);
+  }
+
+  return $sguid;
+}
+
+
+/**
+ * Notify users about the new string that was added.
+ */
+function _btr_new_string_notification($project, $string, $sguid, $uid) {
+  $importer = user_load($uid);
+
+  // Get all the users interested on this project.
+  $uids = \db_query(
+    "SELECT DISTINCT entity_id
+     FROM {field_data_field_projects}
+     WHERE field_projects_value = :project",
+    [ ':project' => "vocabulary/$project" ]
+  )->fetchCol();
+  $users = user_load_multiple($uids);
+
+  // Notify the users about the new term.
+  $notifications = array();
+  foreach ($users as $key => $user) {
+    $params = array(
+      'type' => 'notify-on-new-vocabulary-term',
+      'uid' => $user->uid,
+      'username' => $user->name,
+      'recipient' => $user->name . ' <' . $user->mail . '>',
+      'project' => $project,
+      'string' => $string,
+      'sguid' => $sguid,
+      'author' => $importer->name,
+    );
+    $notifications[] = $params;
+  }
+  btr::queue('notifications', $notifications);
+}
---
+++ modules/custom/btrCore/lib/fn/vocabulary/string_del.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @file
+ * Definition of function vocabulary_string_del().
+ */
+
+namespace BTranslator;
+use \btr;
+
+/**
+ * Delete a string from a vocabulary.
+ *
+ * @param $name
+ *   The name of the vocabulary.
+ *
+ * @param $lng
+ *   The language of the vocabulary.
+ *
+ * @param $sguid
+ *   ID of the string to be deleted.
+ *
+ * @return
+ *   FALSE if permissions are not sufficient, otherwise TRUE
+ */
+function vocabulary_string_del($name, $lng, $sguid) {
+  // Set project variables.
+  $origin = 'vocabulary';
+  $project = $name . '_' . $lng;
+
+  // Only a project admin can delete strings.
+  if (!btr::user_is_project_admin($origin, $project))  return FALSE;
+
+  // Remove the string from the 'materialized view' table of the project.
+  $q = 'SELECT string FROM {btr_strings} WHERE sguid = :sguid';
+  $string = btr::db_query($q, [':sguid' => $sguid])->fetchField();
+  $table = 'btr_mv_' . strtolower($project);
+  btr::db_delete($table)->condition('string', $string)->execute();
+
+  // Get the template of the project.
+  $pguid = sha1($origin . $project);
+  $q = 'SELECT potid FROM {btr_templates} WHERE pguid = :pguid';
+  $potid = btr::db_query($q, [':pguid' => $pguid])->fetchField();
+
+  // Remove the string from the template of the project.
+  btr::db_delete('btr_locations')
+    ->condition('potid', $potid)
+    ->condition('sguid', $sguid)
+    ->execute();
+
+  // Delete any translations of the string.
+  $q = 'SELECT tguid FROM {btr_translations} WHERE sguid = :sguid';
+  $tguid_list = btr::db_query($q, [':sguid' => $sguid])->fetchCol();
+  foreach ($tguid_list as $tguid) {
+    btr::translation_del($tguid, FALSE);
+  }
+
+  // Delete the string itself.
+  btr::db_delete('btr_strings')->condition('sguid', $sguid)->execute();
+
+  return TRUE;
+}
