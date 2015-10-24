f63c67b27d0c52e780d89a9cefd52b7a7c9c4b5c
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-24 09:16:10 +0200

    Bug fix

--- install/scripts/system-config.sh
+++ install/scripts/system-config.sh
@@ -131,6 +131,6 @@ then
 // Don't expire login quickly
 \$sessionDuration = 60*60*24*7; // 60*60*24*7 = one week
 ini_set('session.gc_maxlifetime', \$sessionDuration);
-$cfg['LoginCookieValidity'] = \$sessionDuration;
+\$cfg['LoginCookieValidity'] = \$sessionDuration;
 EOF
 fi
