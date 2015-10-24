50889d963406562dd307169a80670c603be40eae
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-24 22:54:02 +0000

    Update README files.

--- modules/custom/btrCore/data/install.sh
+++ modules/custom/btrCore/data/install.sh
@@ -21,17 +21,19 @@ test "$development" != 'false' && test/update.sh
 
 dir=$(dirname $0)
 echo "
-In order to import the real data, first of all check/modify
-    $dir/config.sh
-and then run
-    $dir/update.sh
+In order to import the real data, first of all edit:
 
-Importing all the data can take a lot of time (many hours,
-maybe days), so it is a good idea to run the update script
-like this:
-    nohup nice $dir/update.sh &
+    /var/www/data/config.sh
 
-Then you can check the progress at any time by:
-    tail -f nohup.out
+and then run:
+
+    /var/www/data/get.sh
+    tail -f /var/www/data/nohup-get.out
+
+    /var/www/data/import.sh
+    tail -f /var/www/data/nohup-import.out
+
+Downloading and importing all the projects can take
+a lot of time (many hours, maybe days).
 
 "
