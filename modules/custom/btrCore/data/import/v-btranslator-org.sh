80d3150cc8b9022f27103c136a69cc92dd56b936
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-24 22:55:39 +0000

    Add language es to the translations of v.btranslator.org

--- modules/custom/btrCore/data/import/v-btranslator-org.sh
+++ modules/custom/btrCore/data/import/v-btranslator-org.sh
@@ -20,7 +20,9 @@ echo -e "\n==========> $origin $project"
 drush @btr btrp-add $origin $project $data_root/vocabulary-jquery/l10n/app.pot
 drush @btr btrp-import $origin $project sq $data_root/vocabulary-jquery/l10n/po/sq.po
 drush @btr btrp-import $origin $project de $data_root/vocabulary-jquery/l10n/po/de.po
+drush @btr btrp-import $origin $project es $data_root/vocabulary-jquery/l10n/po/es.po
 
 ### set the author of translations
 drush @btr btr-vote --user="Dashamir Hoxha" sq $data_root/vocabulary-jquery/l10n/po/sq.po
 drush @btr btr-vote --user="OpenSrcKansas"  de $data_root/vocabulary-jquery/l10n/po/de.po
+drush @btr btr-vote --user="jrosgiralt"     es $data_root/vocabulary-jquery/l10n/po/es.po
