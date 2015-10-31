2670e5aaceaf70f2667b1238c8deb01d04cd1735
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-31 20:10:26 +0000

    Fix the script for importing Drupal modules.

--- modules/custom/btrCore/data/import/drupal.sh
+++ modules/custom/btrCore/data/import/drupal.sh
@@ -11,18 +11,24 @@ cd $(dirname $0)
 origin=Drupal
 po_dir=$data_root/Drupal
 
+### import POT files
+pot_files=$(find $po_dir -name "*\.fr\.po")
+for pot_file in $pot_files
+do
+    filename=$(basename $pot_file)
+    project=${filename%%-*.fr.po}
+    $drush btrp-add $origin $project $pot_file
+done
+
 ### import PO files of each language
 for lng in $languages
 do
-    echo -e "\n==========> $origin $lng "
     po_files=$(find $po_dir -name "*\.$lng\.po")
     for po_file in $po_files
     do
         filename=$(basename $po_file)
         project=${filename%%-*.$lng.po}
         echo -e "\n----------> $origin $project $lng "  # ;  continue;  ## debug
-        ### import the template and the translation files
-        $drush btrp-add $origin $project $po_file
         $drush btrp-import $origin $project $lng $po_file
     done
 done
