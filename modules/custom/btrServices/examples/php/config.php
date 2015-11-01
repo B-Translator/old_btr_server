8e9f895ede639b4c34864f1ec8f6ed0ef8c03123
Dashamir Hoxha <dashohoxha@gmail.com>
2015-11-01 16:01:32 +0100

    Fix the config of api examples.

--- modules/custom/btrServices/examples/php/config.php
+++ modules/custom/btrServices/examples/php/config.php
@@ -6,13 +6,13 @@ if (php_sapi_name() != "cli") {
 
 define('DEBUG', TRUE);
 
-$base_url = 'https://dev.btranslator.org';
+$base_url = 'http://dev.btranslator.org';
 //$base_url = 'https://dev.btr.example.org';
 
 $auth = array(
   'token_url' => $base_url . '/oauth2/token',
-  'client_id' => 'emberjs',
-  'client_secret' => '123456',
+  'client_id' => 'test1',
+  'client_secret' => '12345',
   'username' => 'user1',
   'password' => 'pass1',
   'scope' => 'user_profile',
