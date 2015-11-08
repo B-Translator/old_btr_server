9fa17c5141d89526f90f930150d266857303e077
Dashamir Hoxha <dashohoxha@gmail.com>
2015-11-08 10:23:08 +0000

    Rename table btr_translation_projects to translation_projects

--- modules/custom/btrCore/lib/fn/utils/get_app_url.php
+++ modules/custom/btrCore/lib/fn/utils/get_app_url.php
@@ -11,7 +11,7 @@ namespace BTranslator;
  */
 function utils_get_app_url($lng, $origin, $project) {
   list($_, $lng) = explode('_', $project);
-  $query = 'SELECT app_url FROM {btr_translation_projects}
+  $query = 'SELECT app_url FROM {translation_projects}
             WHERE lng = :lng AND origin = :origin AND project = :project';
   $params = array(':lng' => $lng, ':origin' => $origin, ':project' => $project);
   $app_url = \db_query($query, $params)->fetchField();
